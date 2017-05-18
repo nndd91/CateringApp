f3e356f1def5dd1faa52b48f33a62ecbe2e0060e
nndd91 <nndd91@hotmail.com>
2017-05-18 18:08:04 +0800

    Orders controller update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,8 +12,22 @@ class OrdersController < ApplicationController
   def new 
     @order = Order.new
     @menus = Menu.all
+    @priceplan = Priceplan.all
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to orders_url
+    else
+      render :new
+    end
+  end
+
+  private
+  def order_params
+    params.require(:orders).permit(:custname, :contactnumber, :custaddress, :email, :deliveryDate)
   end
   
-
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Menu.create(item: "Chicken Wing", description: "Delicious wings", img_location: "chicken_wing.jpg")
-Menu.create(item: "Burger", description: "Delicious Burger", img_location: "burger.jpg")
-Menu.create(item: "Fries", description: "Delicious Fries", img_location: "fries.jpg")
+Menu.create(item: "Chicken Wing", description: "Delicious wings", img_location: "chicken_wing.jpg", itemtag: "chicken_wing")
+Menu.create(item: "Burger", description: "Delicious Burger", img_location: "burger.jpg", itemtag: "burger")
+Menu.create(item: "Fries", description: "Delicious Fries", img_location: "fries.jpg", itemtag: "fries")
+Priceplan.create(no_of_people: 10, no_of_dishes: 6, price: "100", add_item_cost: "10")
+Priceplan.create(no_of_people: 20, no_of_dishes: 8, price: "200", add_item_cost: "20")
+Priceplan.create(no_of_people: 30, no_of_dishes: 10, price: "300", add_item_cost: "30")
