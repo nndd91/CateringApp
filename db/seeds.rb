b906aa6a228328d75b165fa0675769e123cbd703
nndd91 <nndd91@hotmail.com>
2017-05-18 23:20:09 +0800

    Removed redundant code in seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,10 +16,4 @@ Priceplan.create(no_of_people: 30, no_of_dishes: 10, price: "300", add_item_cost
 Itemlist.create(itemname: "Chicken Wing", itemtag: "chicken_wing", order_id: 1)
 Itemlist.create(itemname: "Burger", itemtag: "burger", order_id: 1)
 
-    t.integer "order_id"
-    t.string "itemname"
-    t.integer "orderid"
-    t.string "itemtag"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_itemlists_on_order_id"
+
