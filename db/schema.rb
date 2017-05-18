db5682b2b9a1d6873c963b2a8645697135cff3ab
nndd91 <nndd91@hotmail.com>
2017-05-18 17:50:07 +0800

    Finished migration and adding column itemtag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518093802) do
+ActiveRecord::Schema.define(version: 20170518094629) do
 
   create_table "menus", force: :cascade do |t|
     t.string "item"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170518093802) do
     t.string "img_location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "itemtag"
   end
 
   create_table "orders", force: :cascade do |t|
