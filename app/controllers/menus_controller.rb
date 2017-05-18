547f2f9395e16fe6b0e2573e74c5459f05f7d0ea
nndd91 <nndd91@hotmail.com>
2017-05-18 17:07:25 +0800

    finished menu create

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,24 @@
+class MenusController < ApplicationController
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to menus_url
+    else
+      render :new
+    end
+    
+  end
+
+
+  private
+  def menu_params
+    params.require(:menu).permit(:item, :description, :img_location)
+  end
+  
+
+end
---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,18 @@
+class OrdersController < ApplicationController
+
+  def index
+
+  end
+
+  def show #Show order for each user
+
+  end
+  
+  
+  def new 
+    
+  end
+  
+
+
+end
